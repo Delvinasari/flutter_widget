760530e995d12060058cf8b046ac89c2e64df527
Delvinasari <163649432+Delvinasari@users.noreply.github.com>
2024-03-20 23:49:50 +0700

    Add files via upload

---
+++ text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello world"),
+        ),
+        body: Center(
+          child: Container(
+           color: Colors.red,
+           width: 150,
+           height: 50,
+           child: Text(
+            "Ini adalah text fluter widget.",
+            textAlign: TextAlign.center,
+            ))),
+      )
+    );
+  } 
+}
